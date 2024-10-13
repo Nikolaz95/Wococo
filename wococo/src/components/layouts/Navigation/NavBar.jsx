6bd881d23ad819dbb94f78759f332db392c31adc
Nikola Zovko <nikolajoe95@gmail.com>
2024-10-13 15:23:15 +0200

    update-1.002

--- wococo/src/components/layouts/Navigation/NavBar.jsx
+++ wococo/src/components/layouts/Navigation/NavBar.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 
 //import css
 import "./NavBar.css"
 
+
+//import img
+import AvatarDefoult from "../../../assets/img/icon2-user.png";
+
+
 const NavBar = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const user = "John Doe";
+
+
+
     return (
         <div className="navigationContent">
             <ul>
-                {/* <li>
-                    <NavLink to="/register">
-                        Sing up
-                    </NavLink>
-                </li> */}
-
-                <li>
-                    <NavLink to="/singIn">
-                        singIn
-                    </NavLink>
-                </li>
+                {isLoggedIn ? (
+                    <li>
+                        <div className="UserNavbar">
+                            <figure>
+                                <img src={AvatarDefoult} className="userIcon" alt={user} />
+                            </figure>
+                            <p>{user}</p>
+                        </div>
+                    </li>
+                ) : (
+                    <li>
+                        <NavLink to="/register">
+                            Sign In
+                        </NavLink>
+                    </li>
+                )}
             </ul>
         </div>
     )
